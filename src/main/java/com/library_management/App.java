e0a37be26a53cabaa577eb5c8265e95764990a53
tarun sasapu <tarun.sasapu@example.com>
2025-07-24 10:53:06 +0530

    HomePage UI

--- src/main/java/com/library_management/App.java
+++ src/main/java/com/library_management/App.java
@@ -8,21 +8,18 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("home"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
+  static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
