4e0abc4a495716a35b0e4ce90a235a137f9d365c
tarun sasapu <tarun.sasapu@example.com>
2025-07-24 19:05:13 +0530

    made changes

--- src/main/java/com/library_management/domain/Member.java
+++ src/main/java/com/library_management/domain/Member.java
@@ -1,5 +1,55 @@
 package com.library_management.domain;
 
 public class Member {
+	private int memberId;
+	private String memberName;
+	private String memberMail;
+	private String gender;
+	private String memberAddress;
+	public int getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public String getMemberMail() {
+		return memberMail;
+	}
+	public void setMemberMail(String memberMail) {
+		this.memberMail = memberMail;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMemberAddress() {
+		return memberAddress;
+	}
+	public void setMemberAddress(String memberAddress) {
+		this.memberAddress = memberAddress;
+	}
+	
+	
+	public Member(int memberId, String memberName, String memberMail, String gender, String memberAddress) {
+		this.memberId = memberId;
+		this.memberName = memberName;
+		this.memberMail = memberMail;
+		this.gender = gender;
+		this.memberAddress = memberAddress;
+	}
+	@Override
+	public String toString() {
+		return "Member [memberId=" + memberId + ", memberName=" + memberName + ", memberMail=" + memberMail
+				+ ", gender=" + gender + ", memberAddress=" + memberAddress + "]";
+	}
+	
 
 }
