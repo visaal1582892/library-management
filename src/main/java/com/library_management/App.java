99dd92615241015e943895b9214185d95b2147a2
Datla Rohit Varma <visaal1582892@gmail.com>
2025-07-28 09:46:00 +0530

    written some tests

--- src/main/java/com/library_management/App.java
+++ src/main/java/com/library_management/App.java
@@ -36,6 +36,8 @@ public class App extends Application {
     public static void main(String[] args) {
     	DBConnection.connectToDB("jdbc:mysql://localhost:3306/lms");
         launch();
+        DBConnection.closeStatement();
+        DBConnection.closeConn();
     }
 
 }
