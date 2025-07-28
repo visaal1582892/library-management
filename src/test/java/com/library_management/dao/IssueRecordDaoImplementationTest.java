e732cdc0f55465d993ae2be0f088c86a422e8f7f
Allegapally sahithi <allegapallysahithi@gmail.com>
2025-07-28 10:42:10 +0530

    changes

--- src/test/java/com/library_management/dao/IssueRecordDaoImplementationTest.java
+++ src/test/java/com/library_management/dao/IssueRecordDaoImplementationTest.java
@@ -2,23 +2,36 @@ package com.library_management.dao;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
+import com.library_management.dao.implementation.IssueRecordDAOImplementation;
+import com.library_management.domain.IssueRecord;
 
 class IssueRecordDAOImplementationTest {
 
 	@Test
 	void testIssueBook() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		IssueRecord issue = new IssueRecord(1, 1, LocalDate.now());
+		String print_message = new IssueRecordDAOImplementation().issueBook(issue);
+		assertEquals("Book issued successfully",print_message);
 	}
 
 	@Test
 	void testReturnBook() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		String print_message = new IssueRecordDAOImplementation().returnBook(1, 1);
+		assertEquals("Book returned successfully",print_message);
 	}
 
 	@Test
 	void testGetAllIssues() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		List<IssueRecord> all = new IssueRecordDAOImplementation().getAllIssues();
+		assertNotNull(all);
 	}
 
 }
--- src/test/java/com/library_management/services/IssueRecordServiceImplementationTest.java
+++ src/test/java/com/library_management/services/IssueRecordServiceImplementationTest.java
@@ -2,33 +2,45 @@ package com.library_management.services;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
+import com.library_management.dao.implementation.IssueRecordDAOImplementation;
+import com.library_management.domain.IssueRecord;
+import com.library_management.services.implementation.IssueRecordServiceImplementation;
 
 class IssueRecordServiceImplementationTest {
 
 	@Test
 	void testIssueBook() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	void testReturnBook() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	void testGetAllIssues() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		List<IssueRecord> all = new IssueRecordServiceImplementation().getAllIssues();
+		assertNotNull(all);
 	}
 
 	@Test
 	void testGetOverdueBooks() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		List<IssueRecord> overdue = new IssueRecordServiceImplementation().getOverdueBooks();
+        assertNotNull(overdue);
 	}
 
 	@Test
 	void testGetActiveIssuedBooks() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		List<List<String>> activeIssues = new IssueRecordServiceImplementation().getActiveIssuedBooks();
+		assertNotNull(activeIssues);
 	}
 
 }
